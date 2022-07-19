6b18d4ef13d97ca2255c46b97a96592b406d62c1
Vadad Rzali <vedad.rzali@gmail.com>
2022-07-18 22:28:14 -0400

    Added server

---
+++ server.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const express = require('express');
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.static('public'));
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.listen(PORT, () => {
+    console.log(`API Server now on port ${PORT}!`);
+});
