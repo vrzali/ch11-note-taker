3d8387db07e3a26027bd4d4f53fac9ebeda4286e
Vadad Rzali <vedad.rzali@gmail.com>
2022-07-19 22:02:28 -0400

    Added routes and updated notes code

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const {createNewNotes} = require("../../lib/notes");
+const {notes} = require("../../data/db.json");
 
-router.use(require('./notesRoutes'));
+router.get("/notes",(req,res)=>{
+  let results=notes;
+  res.json(results);
+});
+
+router.post("/notes", (req, res) => {
+  let noteReceived = req.body;
+  noteReceived.id = notes.length+1;
+  const note = createNewNotes(noteReceived, notes);
+  res.json(note);
+});
 
 module.exports = router;
